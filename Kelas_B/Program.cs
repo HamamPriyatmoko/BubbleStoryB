c33f33837793eb3dd4853aba89d5472a29a49795
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-10-06 17:17:57 +0700

    Add project files.

---
+++ Kelas_B/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variable int untuk menyimpan banyaknya data pada array
+        private int[] n;
+
+        // Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            // Menerima angka untuk menetukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya ")
+            }
+        }
+    }
+}
