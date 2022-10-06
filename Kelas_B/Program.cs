f8b550d6f453757d3df829930e1e708d7f8a149f
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-10-06 18:11:54 +0700

    menghapus tanda kurung "[]"

--- Kelas_B/Program.cs
+++ Kelas_B/Program.cs
@@ -8,7 +8,7 @@ namespace Bubble_Sort
         private int[] a = new int[20];
 
         // Deklarasi variable int untuk menyimpan banyaknya data pada array
-        private int[] n;
+        private int n;
 
         // Fungsi / Method untuk menerima masukan
         public void read()
@@ -16,7 +16,25 @@ namespace Bubble_Sort
             // Menerima angka untuk menetukan banyaknya data yang disimpan pada array
             while (true)
             {
-                Console.Write("Masukkan banyaknya ")
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Masukkan elemen array");
+            Console.WriteLine("---------------------");
+
+            // Pengguna memasukan elemen pada array
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
             }
         }
     }
